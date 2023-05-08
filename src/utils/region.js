9a31a8ee0bcc8d9a3a5bb36aae423118e2dd1caa
穗乃果 <1763575579@qq.com>
2023-05-08 21:06:19 +0800

    优化使用体验

--- src/utils/region.js
+++ src/utils/region.js
@@ -3,6 +3,7 @@ import eu from "@/assets/images/regions/eu.png";
 import oc from "@/assets/images/regions/oc.png";
 import nam from "@/assets/images/regions/nam.png";
 import sam from "@/assets/images/regions/sam.png";
+import afr from "@/assets/images/regions/afr.png";
 
 function getRegionImage(region) {
   switch (region) {
@@ -16,6 +17,8 @@ function getRegionImage(region) {
       return nam;
     case "sam":
       return sam;
+    case "afr":
+      return afr;
     default:
       return region;
   }
