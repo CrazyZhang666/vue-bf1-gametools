e53e4a6f7fcf1ae4febd065d25bb4f3fba97ce5d
穗乃果 <1763575579@qq.com>
2023-05-09 10:42:46 +0800

    修复bug

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import autoprefixer from "autoprefixer";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
     },
   },
 });
